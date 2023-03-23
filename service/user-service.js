d66ecdf7d1a050b61c06fad66ba392be8f23e596
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-03-23 20:22:49 +0200

    add user controller and user service

--- service/user-service.js
+++ service/user-service.js
@@ -1,27 +1,31 @@
 // const UserModel = require('../models/user-model');
 const bcrypt = require('bcrypt');
+const { v4: uuidv4 } = require('uuid');
 const tokenService = require('./token-service');
 const UserDto = require('../dtos/user-dto');
 const ApiError = require('../exceptions/api.error');
 const db = require('../db');
 
 class UserService {
+
   async registration(name, email, password) {
     const candidate = await db.query(`SELECT email FROM users WHERE email = $1`, [email]);
+
     if(typeof candidate.rows[0] !== 'undefined') {
       throw ApiError.BadRequest(`User with email ${email} already exist`);
     }
 
+    const id = uuidv4();
     const hashPassword = await bcrypt.hash(password, 3);
 
-    const newUser = await db.query(`INSERT INTO users (name, email, password)
-                                    VALUES ($1, $2, $3) 
-                                    RETURNING name, email`, 
-                                    [name, email, hashPassword]);
-    
-    const userDto = new UserDto({name: name, email: email});
+    const newUser = await db.query(`INSERT INTO users (id, name, email, password)
+                                    VALUES ($1, $2, $3, $4) 
+                                    RETURNING id, name, email`, 
+                                    [id, name, email, hashPassword]);
+
+    const userDto = new UserDto(newUser.rows[0]);
     const tokens = tokenService.generateTokens({...userDto});
-    // await tokenService.saveToken(userDto.id, tokens.refreshToken);
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
     
     return {
       ...tokens,
@@ -29,24 +33,36 @@ class UserService {
     }
   }
 
-  // async login(email, password) {
-  //   const user = await UserModel.findOne({email})
-  //   if(!user) {
-  //     throw ApiError.BadRequest('Пользователь с таким имейлом не найден');
-  //   }
-  //   const usPassEquals = await bcrypt.compare(password, user.password);
-  //   if(!usPassEquals) {
-  //     throw ApiError.BadRequest('Пароль неверный');
-  //   }
-  //   const userDto = new UserDto(user);
-  //   const tokens = tokenService.generateTokens({...userDto});
+  async login(email, password) {
+    const user = await db.query(`SELECT id, name, email, password 
+                                 FROM users 
+                                 WHERE email = $1`,
+                                 [email]);
+                                 
+    if(typeof user.rows[0] === 'undefined') {
+      throw ApiError.BadRequest('User with this email address not found');
+    }
 
-  //   await tokenService.saveToken(userDto.id, tokens.refreshToken);
-  //   return {
-  //     ...tokens,
-  //     user: userDto
-  //   }
-  // }
+    const userData = user.rows[0]
+
+    const usPassEquals = await bcrypt.compare(password, userData.password);
+    if(!usPassEquals) {
+      throw ApiError.BadRequest('You have entered an incorrect password.');
+    }
+
+    const userDto = new UserDto({id: userData.id,
+                                 name: userData.name,
+                                 email: userData.email
+                                });
+
+    const tokens = tokenService.generateTokens({...userDto});
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return {
+      ...tokens,
+      user: userDto
+    }
+  }
 
   // async logout(refreshToken) {
   //   const token = await tokenService.removeToken(refreshToken);
