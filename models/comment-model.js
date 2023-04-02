c0a48263c77bde6ffdc2240d349ec62934021b0c
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-04-02 13:11:39 +0300

    add comment model

--- models/comment-model.js
+++ models/comment-model.js
@@ -2,13 +2,18 @@ const sequelize = require('../database/db');
 const {DataTypes} = require('sequelize');
 
 const Comment = sequelize.define('comment', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
   content: {
     type: DataTypes.TEXT,
     allowNull: false
   },
   displayOrder: {
     type: DataTypes.INTEGER,
-    allowNull: false
+    allowNull: false,
   },
   indentLevel: {
     type: DataTypes.INTEGER,
