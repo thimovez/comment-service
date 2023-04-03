42333019b5f3e16d9429dc78e34bfe947012a850
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-04-03 14:20:47 +0300

    update db tables

--- database.sql
+++ database.sql
@@ -1,100 +1,38 @@
-create TABLE users(
-  id VARCHAR PRIMARY KEY UNIQUE NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL ,
-  img VARCHAR(255),
-  password VARCHAR(255)
+CREATE TABLE "users" (
+  "id" varchar(255) PRIMARY KEY NOT NULL,
+  "firstName" varchar(255) NOT NULL,
+  "email" varchar(255) UNIQUE NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "updated_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE TABLE tokens(
-    id VARCHAR PRIMARY KEY UNIQUE REFERENCES users(id),
-    refreshToken VARCHAR(255) UNIQUE NOT NULL DEFERRABLE
-    constraint pk_refreshToken primary key (refreshToken) deferrable initially immediate
+CREATE TABLE "tokens" (
+  "id" varchar(255) PRIMARY KEY NOT NULL,
+  "refreshToken" varchar(255) NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "updated_at" timestamptz NOT NULL DEFAULT (now()),
+  "user_id" varchar(255)
 );
 
-create TABLE comments(
-  id SERIAL PRIMARY KEY,
-  content VARCHAR(255) NOT NULL,
-  user_id INTEGER,
-  FOREIGN KEY (user_id) REFERENCES users (id)
+CREATE TABLE "comments" (
+  "id" serial4 PRIMARY KEY NOT NULL,
+  "content" varchar NOT NULL,
+  "user_id" varchar(255) NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "updated_at" timestamptz NOT NULL DEFAULT ('0000-00-00 00:00:00')
 );
 
-create TABLE parent_child_comment(
-  parent_comment_id INTEGER
-  child_comment_id INTEGER
-  FOREIGN KEY (parent_comment_id) REFERENCES comments (id)
+CREATE TABLE "comments_path" (
+  "ancestor" integer NOT NULL,
+  "descendant" integer NOT NULL,
+  "path_length" integer DEFAULT 0,
+  PRIMARY KEY ("ancestor", "descendant")
 );
 
+ALTER TABLE "comments" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
+ALTER TABLE "users" ADD FOREIGN KEY ("id") REFERENCES "tokens" ("user_id");
 
-CREATE TABLE topics (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    content TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
+ALTER TABLE "comments_path" ADD FOREIGN KEY ("ancestor") REFERENCES "comments" ("id");
 
-CREATE TABLE posts (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    topic_id INTEGER NOT NULL,
-    content TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (topic_id) REFERENCES topics(id) ON DELETE CASCADE
-);
-
-CREATE TABLE tags (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) UNIQUE NOT NULL
-);
-
-CREATE TABLE topic_tags (
-    topic_id INTEGER NOT NULL,
-    tag_id INTEGER NOT NULL,
-    PRIMARY KEY (topic_id, tag_id),
-    FOREIGN KEY (topic_id) REFERENCES topics(id) ON DELETE CASCADE,
-    FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
-);
-
-
-
-created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-
-CREATE TABLE comments (
-  id SERIAL PRIMARY KEY,
-  content TEXT NOT NULL,
-  parent_id INTEGER,
-  ancestor_id INTEGER,
-  depth INTEGER
-);
-
-CREATE INDEX comments_ancestor_index ON comments (ancestor_id);
-
-INSERT INTO comments (content, parent_id, ancestor_id, depth)
-VALUES ('First comment', NULL, 1, 0),
-       ('Reply to first comment', 1, 1, 1),
-       ('Second comment', NULL, 3, 0),
-       ('Reply to second comment', 3, 3, 1),
-       ('Reply to reply', 2, 1, 2);
-
-
-CREATE EXTENSION ltree;
-
-CREATE TABLE comments (
-    id INTEGER PRIMARY KEY NOT NULL,
-    content TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    parent_path LTREE,
-    user_id VARCHAR NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
+ALTER TABLE "comments_path" ADD FOREIGN KEY ("descendant") REFERENCES "comments" ("id");
