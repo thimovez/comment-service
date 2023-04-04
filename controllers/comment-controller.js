6f030858920717aedb6bc3a21c06be31108f3673
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-04-04 12:01:16 +0300

    combined reply and comment handlers into one

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,27 +1,19 @@
 const commentService = require('../service/comment-service');
 
 class CommentController {
-  //обьеденить createComment and createReply
   async createComment(req, res, next) {
     try {
       const user = req.user;
-      const {content} = req.body;
-      const newComment = await commentService.createComment(content, user.id);
-      
-      res.json(newComment);
-    } catch(e) {
-        next(e);
-    }
-  }
-
-  async createReply(req, res, next) {
-    try {
-      const user = req.user;
       const {id, content} = req.body;
+      if (!id) {
+        const newComment = await commentService.createComment(content, user.id);
 
+        res.json(newComment);
+      }
+      
       const newReply = await commentService.createReply(id, content, user.id );
 
-      res.json(newReply)
+      res.json(newReply);
     } catch(e) {
         next(e);
     }
