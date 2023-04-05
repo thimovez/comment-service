90e195009ca906202efd1fdb1fac02464d454d36
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-04-05 16:16:45 +0300

    return user data to client

--- service/comment-service.js
+++ service/comment-service.js
@@ -4,17 +4,23 @@ const ApiError = require('../exceptions/api.error');
 const {Comment, CommentPath} = require('../models/comment-model');
 
 class CommentService {
-  async createComment(content, userId) {
-    const comment = await Comment.create({content: content, parentId: userId});
+  async createComment(id, content, user) {
+    if(!id) {
+      const reply = await this.createReply(id, content, user)
+      return reply;
+    }
+    
+    const comment = await Comment.create({content: content, parentId: user.id});
     const path = await CommentPath.create({ancestor: comment.id, descendant: comment.id, path_length: 0});
 
     return {
+      userData: user,
       comment: comment,
       path: path
     }
   }
 
-  async createReply(id, content, userId) {
+  async createReply(id, content, user) {
     const descendant = id;
     const parentComment = await CommentPath.findOne({where: {descendant}});
     if (!parentComment) {
@@ -22,12 +28,13 @@ class CommentService {
     }
 
     const comment_path = parentComment.path_length;
-    const comment = await Comment.create({content: content, parentId: userId});
+    const comment = await Comment.create({content: content, parentId: user.id});
     const path = await CommentPath.create({ancestor: id, 
                                           descendant: comment.id,
                                           path_length: comment_path + 1});
 
     return {
+      userData: user,
       comment: comment,
       path: path
     };
