30c56ba4740e1fdfbf6746b0b7c9c2085b402c60
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-04-10 17:32:18 +0300

    add middleware for create comment handler

--- router/comment-router.js
+++ router/comment-router.js
@@ -2,8 +2,12 @@ const Router = require('express');
 const commentController = require('../controllers/comment-controller');
 const router = new Router();
 const authMiddleware = require('../middleware/auth-middleware');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
-router.post('/', authMiddleware, commentController.createComment);
+const cpUpload = upload.fields([{ name: 'file', maxCount: 1 }, { name: 'img', maxCount: 1 }]);
+
+router.post('/', authMiddleware, cpUpload, commentController.createComment);
 router.post('/sort', authMiddleware, commentController.sortParentComments);
 
 module.exports = router;
