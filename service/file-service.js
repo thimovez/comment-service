6bf7d9b303d529b9ff1f891794fa626acde900c7
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-04-12 14:00:06 +0300

    add basic function to file service

--- service/file-service.js
+++ service/file-service.js
@@ -1,31 +1,49 @@
+const ApiError = require('../exceptions/api.error');
+const sizeOf = require('image-size');
 
 class FileService {
-  verifyFile(file) {
-    if(file === '') {
-      return ''
+  getFileFormat(file) {
+    const mimetypeData = file.mimetype.split('/');
+    const format = mimetypeData[mimetypeData.length - 1];
+
+    return {
+      file: file,
+      format: format
+    };
+  }
+
+  verifyFileFormat(fileData) {
+    const {file, format} = fileData;
+    if(format === 'gif' || format === 'jpg' || format === 'png') {
+      const image = this.verifyImgSize(file);
+
+      return image;
+    }
+
+    if(fileData.format === 'txt') {
+      const text = this.verifyFileSize(file);
+
+      return text;
+    }
+
+    throw ApiError.BadRequest('unexpected file format')
+  }
+
+  verifyFileSize(file) {
+    if (file.size <= 100000) {
+      throw ApiError.BadRequest('file size is so big');
     }
 
     return file;
   }
 
-  verifyFileFormat(file) {
-    if(file === '') {
-      return ''
+  verifyImgSize(file) {
+    const dimensions = sizeOf(file.path);
+    if(dimensions.width > 320 || dimensions.height > 240) {
+      throw ApiError.BadRequest('file is too big');
     }
 
-    
-  }
-
-  verifyImgFormat() {
-
-  }
-
-  verifyFileSize() {
-
-  }
-
-  verifyImgSize() {
-    
+    return file;
   }
 }
 
