b98020d20ae1d065c5e4df80f4c4e7b39c87e255
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-04-28 17:52:51 +0300

    add multer configuration

--- router/comment-router.js
+++ router/comment-router.js
@@ -1,13 +1,43 @@
 'use strict';
 const Router = require('express');
+const path = require('path');
 const commentController = require('../controllers/comment-controller');
 const router = new Router();
 const authMiddleware = require('../middleware/auth-middleware');
 const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const fileService = require('../service/file-service');
 
-router.post('/', authMiddleware,
-  upload.single('file'), commentController.createComment);
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './public/uploads/');
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  }
+});
+
+const upload = multer({
+  storage,
+  fileFilter: (req, file, callback) => {
+    const ext = path.extname(file.originalname);
+    if (ext === '.png' && ext === '.jpg' && ext === '.gif') {
+      fileService.verifyImgSize(file);
+
+      return callback(null, true);
+    }
+
+    if (ext !== '.txt') {
+      fileService.verifyFileSize(file);
+
+      return callback(null, true);
+    }
+
+    callback(new Error('Unexpected file format'), false);
+  }
+}).single('file');
+
+router.post('/', authMiddleware, upload, commentController.createComment);
 router.post('/sort', authMiddleware, commentController.sortParentComments);
 
 module.exports = router;
