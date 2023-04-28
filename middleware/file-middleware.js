8ee49e302df8fdbb88a9b5c7ea2911bb6f6e2ab6
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-04-28 19:14:33 +0300

    add file middleware

---
+++ middleware/file-middleware.js
@@ -0,0 +1,28 @@
+'use strict';
+const ApiError = require('../exceptions/api.error');
+const multer  = require('multer');
+const path = require('node:path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './public/uploads/');
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  }
+});
+
+const upload = multer({
+  storage,
+  fileFilter: (req, file, callback) => {
+    const ext = path.extname(file.originalname);
+    if (ext === '.png' || ext === '.jpg' || ext === '.gif' || ext === '.txt') {
+      return callback(null, true);
+    }
+
+    callback(ApiError('Unexpected file format'), false);
+  }
+}).single('file');
+
+module.exports = upload;
