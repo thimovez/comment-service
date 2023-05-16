ec429aa4f3d5d77c5898ba61d597b0f082700203
Vasyl Kovalenko <kovalenkova26@gmail.com>
2023-05-16 19:09:12 +0300

    add router for gerTreeOfComments

--- router/comment-router.js
+++ router/comment-router.js
@@ -8,6 +8,7 @@ const commentController = require('../controllers/comment-controller');
 router.post('/', authMiddleware, upload, commentController.createComment);
 router.post('/:id', authMiddleware, upload, commentController.createReply);
 router.post('/sort', authMiddleware, commentController.sortParentComments);
+router.get('/:id', authMiddleware, commentController.getTreeOfComments);
 router.delete('/delete/:id', authMiddleware, commentController.deleteComments);
 
 module.exports = router;
