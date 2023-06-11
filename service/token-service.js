f9b78292d03f680b431fc571b7db9874129d4131
Vasyl Kovalenko <thimovez@gmail.com>
2023-06-12 00:10:12 +0300

    add pseudocode for token logic

--- service/token-service.js
+++ service/token-service.js
@@ -20,7 +20,7 @@ class TokenService {
   validateAccessToken(token) {
     try {
       const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
-
+      console.log(userData);
       return userData;
     } catch (e) {
       return null;
@@ -60,6 +60,36 @@ class TokenService {
     const tokenData = await Token.findOne({ where: { refreshToken } });
 
     return tokenData;
+  }
+
+  pushAccessTokenToTrash(accessToken) {
+    /*
+      Проверить валиден ли токен, если да то отравить в помойку
+    */
+    return accessToken;
+  }
+
+  findAccessToken(accessToken) {
+    /*
+      Ищем токен в базе если есть то доступ запрещен по этому токену
+     */
+    return accessToken;
+  }
+
+  countAccessTokensInTrash() {
+    /*
+      Считать количество токенов в базе данных
+      и если условно их больше 5 начинать их удалять
+    */
+    return '';
+  }
+
+  deleteAccessTokenInTrash() {
+    /*
+      Перед тем как удалить - проверить валиден ли токен
+      если да, игнорируем если нет, то удаляем
+    */
+    return '';
   }
 }
 
