0d27b0ac6b8a6fcf26961a0e5074b542bf9ac947
Vasyl Kovalenko <thimovez@gmail.com>
2023-06-26 14:39:16 +0300

    add auth middleware to logout router

--- router/user-routes.js
+++ router/user-routes.js
@@ -1,10 +1,11 @@
 const Router = require('express');
 const userController = require('../controllers/user-controller');
+const authMiddleware = require('../middleware/auth-middleware');
 const router = new Router();
 
 router.post('/registration', userController.registrarion);
 router.post('/login', userController.login);
-router.get('/logout', userController.logout);
+router.get('/logout', authMiddleware, userController.logout);
 router.get('/refresh', userController.refresh);
 
 module.exports = router;
