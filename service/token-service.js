ded2804ebc7dc1db2a5f92aaa897facac5a73dd5
Vasyl Kovalenko <thimovez@gmail.com>
2023-07-16 15:07:53 +0300

    add token expiration to env file

--- service/token-service.js
+++ service/token-service.js
@@ -4,10 +4,10 @@ const { Token, TokenTrash } = require('../models');
 class TokenService {
   generateTokens(payload) {
     const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
-      expiresIn: '2h'
+      expiresIn: process.env.JWT_ACCESS_EXPIRATION
     });
     const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
-      expiresIn: '30d'
+      expiresIn: process.env.JWT_REFRESH_EXPIRATION
     });
 
     return {
