fed0f927b31b035b2f5bcf336ebf3b45b7b1e7c1
Vasyl Kovalenko <thimovez@gmail.com>
2023-07-17 15:24:00 +0300

    refactor

--- router/user-routes.js
+++ router/user-routes.js
@@ -2,8 +2,9 @@ const Router = require('express');
 const userController = require('../controllers/user-controller');
 const authMiddleware = require('../middleware/auth-middleware');
 const router = new Router();
+const { regUserRules } = require('../utils/validate-rules');
 
-router.post('/registration', userController.registrarion);
+router.post('/registration', regUserRules, userController.registrarion);
 router.post('/login', userController.login);
 router.get('/logout', authMiddleware, userController.logout);
 router.get('/refresh', userController.refresh);
