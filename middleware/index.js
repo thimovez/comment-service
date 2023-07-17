19325e49a7fcb47eeb875a72ff03ad52c97771fe
Vasyl Kovalenko <thimovez@gmail.com>
2023-07-17 20:41:57 +0300

    refactor

--- middleware/index.js
+++ middleware/index.js
@@ -2,16 +2,12 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const router = require('../router/index');
 const errorMiddleware = require('./error-middleware');
-const { closeConnection } = require('../db/db');
+// const { closeConnection } = require('../db/db');
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use((req, res, next) => {
-  res.on('finish', closeConnection);
-  next();
-});
 app.use('/api', router);
 app.use(errorMiddleware);
 
