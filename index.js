180c93fc79a69818917e869042de569cad26eade
Vasyl Kovalenko <thimovez@gmail.com>
2023-07-17 13:57:17 +0300

    add close connection for sql

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const PORT = process.env.PORT || 5000;
 const start = async () => {
   try {
     await sequelize.authenticate();
-    await sequelize.sync();
     app.listen(PORT, () => {
       console.log(`Server started on http://localhost:${PORT}/`);
     });
+    await sequelize.close();
   } catch (error) {
     console.log(error);
   }
