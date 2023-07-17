72d41d940a3f041da0fb1db1fe41b390dbf97785
Vasyl Kovalenko <thimovez@gmail.com>
2023-07-17 14:53:28 +0300

    remove from main connect to db

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 require('dotenv').config();
-const { sequelize } = require('./models');
 const app = require('./middleware/index');
 
 const PORT = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await sequelize.authenticate();
     app.listen(PORT, () => {
       console.log(`Server started on http://localhost:${PORT}/`);
     });
-    await sequelize.close();
   } catch (error) {
     console.log(error);
   }
