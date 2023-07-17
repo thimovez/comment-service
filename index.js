3ea5c68f24040c459b78c10f4b9747a7a395db8e
Vasyl Kovalenko <thimovez@gmail.com>
2023-07-17 15:21:26 +0300

    open connection with db

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const app = require('./middleware/index');
+const { openConnection } = require('./db/db');
 
 const PORT = process.env.PORT || 5000;
 
 const start = async () => {
   try {
+    await openConnection();
     app.listen(PORT, () => {
       console.log(`Server started on http://localhost:${PORT}/`);
     });
