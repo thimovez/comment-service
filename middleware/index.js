9c9760297f5624f2c099eed7dec834ff340a5a2e
Vasyl Kovalenko <thimovez@gmail.com>
2023-07-17 14:53:43 +0300

    add middleware for close connection

--- middleware/index.js
+++ middleware/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const router = require('../router/index');
 const errorMiddleware = require('./error-middleware');
+const { closeConnection } = require('../models/index');
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use((req, res, next) => {
+  res.on('finish', closeConnection);
+  next();
+});
 app.use('/api', router);
 app.use(errorMiddleware);
 
