6ee46813cdb361658daecbb197c3174a3c2899c1
Vasyl Kovalenko <thimovez@gmail.com>
2023-07-17 15:20:41 +0300

    change path in model

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/db.js')[env];
+const config = require(__dirname + '/../config/db-config.js')[env];
 const db = {};
 
 let sequelize;
