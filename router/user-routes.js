566cb833ef061df99e5d5206cb620028b4bd71c3
Vasyl Kovalenko <thimovez@gmail.com>
2023-07-18 13:45:37 +0300

    add validation middleware to router

--- router/user-routes.js
+++ router/user-routes.js
@@ -3,8 +3,10 @@ const userController = require('../controllers/user-controller');
 const authMiddleware = require('../middleware/auth-middleware');
 const router = new Router();
 const { regUserRules } = require('../utils/validate-rules');
+const  validate  = require('../middleware/validation-middleware');
 
-router.post('/registration', regUserRules, userController.registrarion);
+router.post('/registration',
+  validate(regUserRules), userController.registrarion);
 router.post('/login', userController.login);
 router.get('/logout', authMiddleware, userController.logout);
 router.get('/refresh', userController.refresh);
