7b042bf3e14f45bfa6b2304e165ae85d7130a805
Vasyl Kovalenko <kovalenkova26@gmail.com>
2024-03-22 21:51:15 +0200

    add rate limit for login

--- cm/src/auth/auth.module.ts
+++ cm/src/auth/auth.module.ts
@@ -3,6 +3,8 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -12,8 +14,18 @@ import { JwtModule } from '@nestjs/jwt';
       secret: "secret",
       signOptions: { expiresIn: '60s' },
     }),
+    ThrottlerModule.forRoot([{
+      ttl: 60000,
+      limit: 10,
+    }]),
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ],
   controllers: [AuthController],
   exports: [AuthService]
 })
