f6e3584fd8ebcf34c3ad2ca97e7b8226305ee5aa
Vasyl Kovalenko <kovalenkova26@gmail.com>
2024-03-23 13:32:48 +0200

    add ValidationPipe

--- cm/src/main.ts
+++ cm/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
  const main = async () => {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 
