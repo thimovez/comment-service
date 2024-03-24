bd9da31b1d5785d9d52684f311cacc322fd31762
Vasyl Kovalenko <kovalenkova26@gmail.com>
2024-03-24 11:21:18 +0200

    add password validation

--- cm/src/users/dto/user.dto.ts
+++ cm/src/users/dto/user.dto.ts
@@ -1,9 +1,10 @@
-import { IsEmail, IsNotEmpty, IsNumber } from 'class-validator';
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
 import { User } from '../interfaces/user.interface';
 
 export class CreateUserDTO implements User {
     @IsEmail({}, {message: "Invalid email"})
     readonly email: string;
     @IsNotEmpty()
+    @Length(4, 16, {message: 'Not less than 4 character and not more than 16 character'})
     readonly password: string;
 }
