25fd8233df14ee8067cfe6e56eb59d639f65ec1e
Vasyl Kovalenko <kovalenkova26@gmail.com>
2024-03-26 11:19:46 +0200

    add save refresh token in cookie

--- cm/src/auth/auth.controller.ts
+++ cm/src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { Body, Controller, Post, HttpCode, HttpStatus, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { LoginUserDTO } from './dto/login.user.dto';
 import { SingInResponse } from './interfaces/singInResponse.interface';
@@ -9,7 +10,15 @@ export class AuthController {
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  signIn(@Body() signInDto: LoginUserDTO): Promise<SingInResponse> {
-    return this.authService.signIn(signInDto);
+  signIn(@Body() signInDto: LoginUserDTO, @Res() res: Response): Promise<SingInResponse> {
+    const singInResponse = this.authService.signIn(signInDto);
+
+    res.cookie("refreshToken", 
+    singInResponse.then(v => v.tokens.refresh_token), {
+      maxAge: 60 * 60 * 24 * 7,
+      httpOnly: true
+    });
+
+    return singInResponse;
   }
-}  
+}
