1e7038be2f7f2a31aab5243f9afc1ae362e792a8
Vasyl Kovalenko <kovalenkova26@gmail.com>
2024-03-26 09:07:54 +0200

    add cookie parser middleware

--- cm/src/main.ts
+++ cm/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
  const main = async () => {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
