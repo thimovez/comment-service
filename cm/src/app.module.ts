47fd1a9bad66317c64a6bba449c74628a1a0b999
Vasyl Kovalenko <kovalenkova26@gmail.com>
2024-03-26 18:13:11 +0200

    configure env file in main module

--- cm/src/app.module.ts
+++ cm/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
+    }),
     AuthModule,
     UsersModule
   ],
