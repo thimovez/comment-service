4d27c4d2d51bda810a66deaa820114d208df4752
Vasyl Kovalenko <kovalenkova26@gmail.com>
2024-03-27 14:34:01 +0200

    connect to postgres db

--- cm/src/app.module.ts
+++ cm/src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.development.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+          type: 'postgres',
+          port: configService.get<number>('POSTGRES_PORT'),
+          host: configService.get<string>('POSTGRES_HOST'),
+          password: configService.get<string>('POSTGRES_PASSWORD'),
+          username: configService.get<string>('POSTGRES_USERNAME'),
+          entities: [User],
+          database: configService.get<string>('POSTGRES_DB'),
+          synchronize: true,
+        }),
+    inject: [ConfigService],
     }),
     AuthModule,
     UsersModule
