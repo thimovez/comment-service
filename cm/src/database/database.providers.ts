012c1ca192b98314e331ef4d2487b96d23c49933
Vasyl Kovalenko <kovalenkova26@gmail.com>
2024-03-30 10:44:37 +0200

    change folder of db

---
+++ cm/src/database/database.providers.ts
@@ -0,0 +1,26 @@
+import { ConfigService } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+
+export const checkDirname = () => console.log(__dirname);
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async (configService: ConfigService) => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USERNAME'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        entities: [__dirname + './entities/*.entity.{js,ts}',],
+        migrations: [__dirname + './migrations/*.{js,ts}'],
+        migrationsRun: true,
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
