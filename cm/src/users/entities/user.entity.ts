b64bc5d051d287d2c0f093db013d42b83f1c98ad
Vasyl Kovalenko <kovalenkova26@gmail.com>
2024-08-31 02:19:15 +0300

    describe user entity

--- cm/src/users/entities/user.entity.ts
+++ cm/src/users/entities/user.entity.ts
@@ -1,13 +1,19 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn("uuid")
   id: string;
 
-  @Column()
+  @Column({type: "string", unique: true, nullable: false})
   email: string;
 
-  @Column()
+  @Column({type: "string", length: 60})
   hashed_password: string;
+
+  @CreateDateColumn({type: "timestamp", default: () => 'CURRENT_TIMESTAMP'})
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
 }
