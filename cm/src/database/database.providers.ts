701616e93fefa17d704cde44628357523ff5915b
Vasyl Kovalenko <kovalenkova26@gmail.com>
2024-08-31 22:25:59 +0300

    add path join and trycatch in db provider

--- cm/src/database/database.providers.ts
+++ cm/src/database/database.providers.ts
@@ -1,11 +1,13 @@
 import { ConfigService } from '@nestjs/config';
+import { DATA_SOURCE } from 'src/constants/constants';
 import { DataSource } from 'typeorm';
+import * as path from 'path';
 
 export const checkDirname = () => console.log(__dirname);
 
 export const databaseProviders = [
   {
-    provide: 'DATA_SOURCE',
+    provide: DATA_SOURCE,
     inject: [ConfigService],
     useFactory: async (configService: ConfigService) => {
       const dataSource = new DataSource({
@@ -15,13 +17,18 @@ export const databaseProviders = [
         username: configService.get<string>('POSTGRES_USERNAME'),
         password: configService.get<string>('POSTGRES_PASSWORD'),
         database: configService.get<string>('POSTGRES_DB'),
-        entities: [__dirname + './entities/*.entity.{js,ts}',],
-        migrations: [__dirname + './migrations/*.{js,ts}'],
+        entities: [path.join(__dirname, '../entities/*.entity.{js,ts}')], // Use path.join for cross-platform compatibility
+        migrations: [path.join(__dirname, '../migrations/*.{js,ts}')],
         migrationsRun: true,
         synchronize: true,
       });
 
-      return dataSource.initialize();
+      try {
+        return await dataSource.initialize();
+      } catch (error) {
+        console.error('Error initializing DataSource:', error);
+        throw error; // Re-throw to let NestJS handle it
+      }
     },
   },
 ];
