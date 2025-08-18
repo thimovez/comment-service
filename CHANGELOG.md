23247788b66deb2a4b6d86c63f006feb9568d775
Vasyl Kovalenko <vasyl.kovalenko@globy.com>
2025-08-18 19:17:14 +0300

    removed deprecated express.js code

---
+++ CHANGELOG.md
@@ -0,0 +1,2 @@
+### Changed
+- [Migration from express.js to nest.js](https://github.com/users/thimovez/projects/2/views/1?layout_template=board&pane=issue&itemId=124732168&issue=thimovez%7Ccomment-service%7C3)
--- src/router/index.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express');
-const router = new Router();
-const userRouter = require('./user-routes');
-const commentRouter = require('./comment-router');
-
-router.use('/user', userRouter);
-router.use('/comment', commentRouter);
-
-module.exports = router;
